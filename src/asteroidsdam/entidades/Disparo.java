2f3f5b37f49c6ca1bfc8a5086bb6508fae994b60
cdore <cdore@Equipo>
2018-05-30 20:48:13 +0200

    Comentarios y estructura del código, hecha.

--- src/asteroidsdam/entidades/Disparo.java
+++ src/asteroidsdam/entidades/Disparo.java
@@ -1,63 +1,42 @@
 package asteroidsdam.entidades;
 
 import java.awt.Graphics2D;
-
 import asteroidsdam.Juego;
 import asteroidsdam.Vector;
 
-/**
- * Represents a Disparo within the game world.
- * @author Brendan Jones
- *
- */
 public class Disparo extends Entidad {
-	
-	/**
-	 * The magnitude of the velocity of a Disparo.
-	 */
-	private static final double VELOCITY_MAGNITUDE = 6.75;
-	
-	/**
-	 * The maximum number of cycles that a Disparo can exist.
-	 */
-	private static final int MAX_LIFESPAN = 60;
-	
-	/**
-	 * The number of cycles this Disparo has existed.
-	 */
-	private int lifespan;
+    
+//------------------------------Atributos-------------------------------------//
+    private static final int MAX_CICLOS = 60;
+    private static final double VELOCIDAD = 6.75;
+    private int VIDA_DISPARO;
 
-	/**
-	 * Creates a new Bullet instance.
-	 * @param owner The object that fired the bullet.
-	 * @param angle The direction of the Bullet.
-	 */
-	public Disparo(Entidad owner, double angle) {
-		super(new Vector(owner.position), new Vector(angle).scale(VELOCITY_MAGNITUDE), 2.0, 0);
-		this.lifespan = MAX_LIFESPAN;
-	}
-	
-	@Override
-	public void update(Juego game) {
-		super.update(game);
-		
-		//Decrement the lifespan of the bullet, and remove it if needed.
-		this.lifespan--;
-		if(lifespan <= 0) {
-			flagForRemoval();
-		}
-	}
+//------------------------------Consturctor-----------------------------------//
+    public Disparo(Entidad nave, double angulo) {
+        super(new Vector(nave.pos), new Vector(angulo).scale(VELOCIDAD), 2.0, 0);
+        this.VIDA_DISPARO = MAX_CICLOS;
+    }
 
-	@Override
-	public void handleCollision(Juego game, Entidad other) {
-		if(other.getClass() != Jugador.class) {
-			flagForRemoval();
-		}
-	}
-	
-	@Override
-	public void draw(Graphics2D g, Juego game) {
-		g.drawOval(-1, -1, 2, 2);
-	}
+//----------------------------Métodos públicos--------------------------------//
+    @Override
+    public void actualizar(Juego juego) {
+        super.actualizar(juego);
+        this.VIDA_DISPARO--;
+        if (VIDA_DISPARO <= 0) {
+            marcarParaEliminar();
+        }
+    }
+
+    @Override
+    public void colision(Juego juego, Entidad otroObjeto) {
+        if (otroObjeto.getClass() != Jugador.class) {
+            marcarParaEliminar();
+        }
+    }
+
+    @Override
+    public void pintarEnEspacio(Graphics2D g, Juego juego) {
+        g.drawOval(-1, -1, 2, 2);
+    }
 
 }
